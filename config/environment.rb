23f4b5be11adc53dc7e4a8daa0ecce8df0b0bf16
kasyn <ksn050789@gmail.com>
2014-01-22 09:53:53 +0100

    sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Devi::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['app20074237@heroku.com'],
+    :password       => ENV['8dd7vn2t'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+}
