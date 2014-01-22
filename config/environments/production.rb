b58278bff96e920e3ba0cb34f00453d0474ca106
kasyn <ksn050789@gmail.com>
2014-01-22 13:00:07 +0100

    lol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Devi::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
 
-  }
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'patrykproject.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
